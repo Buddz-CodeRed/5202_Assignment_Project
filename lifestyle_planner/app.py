3b6c7d7de9c7a5f5d16de1ac3f8fc19ec94f54d9
Buddz-CodeRed <badi9code.red@gmail.com>
2025-06-04 00:54:42 +1200

    added daily and monthly listboxes and labels

--- lifestyle_planner/app.py
+++ lifestyle_planner/app.py
@@ -46,6 +46,7 @@ cal.pack(fill='both', expand=True, padx=5, pady=5)
 #   Daily & Monthly lists & Frames
 #------------------------------------------------------>
 
+# frames
 dm_frame = ctk.CTkFrame(main_frame)
 dm_frame.pack(fill='both', anchor='n', padx=5, pady=5)
 
@@ -54,6 +55,34 @@ daily_frame.pack(side='left', fill='both', expand=True, padx=(5, 2.5), pady=5)
 
 month_frame = ctk.CTkFrame(dm_frame)
 month_frame.pack(side='left', fill='both', expand=True, padx=(2.5, 5), pady=5)
+
+# daily label
+daily_title = ctk.CTkLabel(daily_frame, text='My Daily Schedule', font=('Segoe UI', 14))
+daily_title.pack()
+
+# daily list box
+event_list = tk.Listbox(
+    daily_frame,
+    bg="#393A3C",
+    fg="#B3B7B9",
+    highlightbackground='#333333',
+    relief='flat'    
+)
+event_list.pack(fill='both', expand=True)
+
+# month label
+month_title = ctk.CTkLabel(month_frame, text='My Monthly Overview', font=('Segoe UI', 14))
+month_title.pack()
+
+# month list box
+month_list = tk.Listbox(
+    month_frame,
+    bg="#393A3C",
+    fg="#B3B7B9",
+    highlightbackground='#333333',
+    relief='flat'    
+)
+month_list.pack(fill='both', expand=True)
 
 #------------------------------------------------------>
 #   Form/Detail section and Frame
